f1c4432c7e1eaf4e7b8b9bf70563a2d92f12bb1c
Ritik Srivastava <ritik.space@gmail.com>
2019-03-26 13:29:28 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const clear = require('clear');
+const git = require('simple-git/promise')();
+
+var call = 0;
+app.set("view engine", "ejs");
+app.use('/public', express.static('public'));
+
+const dbOptions = { useNewUrlParser: true, reconnectTries: Number.MAX_VALUE, poolSize: 10 };
+mongoose.connect(require("./mongo"), dbOptions).then(
+    () => { console.log(">  Connection Established"); },
+    e => { console.log(">  Connection Failed \n>  " + e); }
+);
+
+app.use(bodyparser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+var Token = mongoose.model("token", new mongoose.Schema({
+    token: String,
+    value: String
+}));
+
+app.get("/git", function(req, res) {
+    var m = req.query.m;
+    console.log("\n" + ++call + ") Pushing to Github");
+    git.add('.')
+        .then(
+            (addSuccess) => {
+                console.log(">  Changes Successfully Added to Stack");
+            }, (failedAdd) => {
+                console.log(">  Changes Adding Failed\n>  " + failedAdd);
+            });
+    git.commit(m)
+        .then(
+            (successCommit) => {
+                console.log(">  Changes Successfully Commited\n   >  Message : \"" + m + "\"");
+            }, (failed) => {
+                console.log(">  Changes Commit Failed\n>  " + failed);
+            });
+    git.push('origin', 'master')
+        .then((success) => {
+            console.log(">  Changes Successfully Pushed to Origin Master");
+        }, (failed) => {
+            console.log(">  Changes Push Failed\n>  " + failed);
+        });
+    res.send("1");
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    clear();
+    console.log("\n" + ++call + ") Starting Server");
+    console.log(">  Server is Listening");
+    console.log("\n" + ++call + ") Connection to MongoDB Atlas Server");
+});
